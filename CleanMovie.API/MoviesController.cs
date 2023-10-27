0010cce695e0a31c1af56fc0f097ee3d6639412a
Imtiaj Ahammad <imtiaj.ahammad@selise.ch>
2023-10-27 10:47:43 +0600

    Add project files.

---
+++ CleanMovie.API/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using CleanMovie.Application;
+using CleanMovie.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CleanMovie.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieService _service;
+
+        public MoviesController(IMovieService service)
+        {
+            _service = service;
+        }
+        // GET: api/<MoviesController>
+        [HttpGet]
+        public ActionResult<List<Movie>> Get()
+        {
+            var moviesFromService = _service.GetAllMovies();
+            return Ok(moviesFromService);
+        }
+    }
+}
---
+++ CleanMovie.Application/IMovieService.cs
@@ -0,0 +1,17 @@
+﻿using CleanMovie.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMovie.Application
+{
+    /// <summary>
+    /// This is a use case
+    /// </summary>
+    public interface IMovieService
+    {
+        List<Movie> GetAllMovies();
+    }
+}
